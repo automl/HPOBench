241a5d65236b813f66aeb47c47467e7b19486a8d
PhMueller <muller-phil@gmx.net>
2020-02-27 18:41:00 +0100

    Update
    - example update
    - one hot encoder

--- hpolib/benchmarks/ml/xgboost_benchmark.py
+++ hpolib/benchmarks/ml/xgboost_benchmark.py
@@ -6,8 +6,9 @@ import numpy as np
 import xgboost as xgb
 from sklearn import pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.metrics import accuracy_score
-from sklearn.metrics import make_scorer
+from sklearn.metrics import accuracy_score, make_scorer
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
 
 import hpolib.util.rng_helper as rng_helper
 from hpolib.abstract_benchmark import AbstractBenchmark
@@ -34,39 +35,44 @@ class XGBoostBenchmark(AbstractBenchmark):
         self.task_id = task_id
         self.accuracy_scorer = make_scorer(accuracy_score)
 
-        self.X_train, self.y_train, self.X_valid, self.y_valid, self.X_test, self.y_test, self.variable_types = \
+        self.X_train, self.y_train, self.X_valid, self.y_valid, self.X_test, self.y_test, variable_types = \
             self.get_data()
 
-        categories = [np.unique(self.X_train[:, i])
-                      if self.variable_types[i] == 'categorical' else []
-                      for i in range(self.X_train.shape[1])]
+        self.categorical_data = np.array([var_type == 'categorical' for var_type in variable_types])
+
+        # Determine all possible values per categorical feature
+        complete_data = np.concatenate([self.X_train, self.X_valid, self.X_test], axis=0)
+        self.categories = [np.unique(complete_data[:, i])
+                           for i in range(self.X_train.shape[1]) if self.categorical_data[i]]
+
+        # Determine the number of categories in the labels.
+        # In case of binary classification `self.num_class` has to be 1 for xgboost.
+        self.num_class = len(np.unique(np.concatenate([self.y_train, self.y_test, self.y_valid])))
+        self.num_class = 1 if self.num_class == 2 else self.num_class
 
         self.train_idx = self.rng.choice(a=np.arange(len(self.X_train)),
                                          size=len(self.X_train),
                                          replace=False)
 
-    def get_data(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray,
-                                np.ndarray, np.ndarray, np.ndarray, List]:
+    def get_data(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, List]:
         """ Loads the data given a task or another source. """
 
-        assert self.task_id is not None, \
-            NotImplementedError('No taskid given. Please either specify a'
-                                'taskid or overwrite the get_data method.')
+        assert self.task_id is not None, NotImplementedError('No task-id given. Please either specify a task-id or '
+                                                             'overwrite the get_data method.')
 
-        dm = OpenMLHoldoutDataManager(openml_task_id=self.task_id,
-                                      rng=self.rng)
+        dm = OpenMLHoldoutDataManager(openml_task_id=self.task_id, rng=self.rng)
         X_train, y_train, X_val, y_val, X_test, y_test = dm.load()
 
         return X_train, y_train, X_val, y_val, X_test, y_test, dm.variable_types
 
     def shuffle_data(self, rng=None):
-        """ Reshuffle the training data. """
+        """ Reshuffle the training data. If 'rng' is None, the training idx are shuffled according to the
+        class-random-state"""
         random_state = rng_helper.get_rng(rng, self.rng)
         random_state.shuffle(self.train_idx)
 
     @AbstractBenchmark._check_configuration
-    def objective_function(self, config: Dict, n_estimators: int,
-                           subsample: float, **kwargs) -> Dict:
+    def objective_function(self, config: Dict, n_estimators: int, subsample: float, **kwargs) -> Dict:
         """
         Trains a XGBoost model given a hyperparameter configuration and
         evaluates the model on the validation set.
@@ -94,26 +100,19 @@ class XGBoostBenchmark(AbstractBenchmark):
             subsample : fraction which was used to subsample the training data
 
         """
-        assert 0 < subsample <= 1, \
-            ValueError(f'Parameter \'subsample\' must be in range (0, 1] '
-                       f'but was {subsample}')
+        assert 0 < subsample <= 1, ValueError(f'Parameter \'subsample\' must be in range (0, 1] but was {subsample}')
 
         rng = kwargs.get('rng', None)
         self.rng = rng_helper.get_rng(rng=rng, self_rng=self.rng)
         start = time.time()
 
-        model = self._get_pipeline(n_estimators=n_estimators, **config)
-
         train_idx = self.train_idx[:int(len(self.train_idx) * subsample)]
 
+        model = self._get_pipeline(n_estimators=n_estimators, **config)
         model.fit(X=self.X_train[train_idx], y=self.y_train[train_idx])
 
-        train_loss = 1 - self.accuracy_scorer(model,
-                                              self.X_train[train_idx],
-                                              self.y_train[train_idx])
-        val_loss = 1 - self.accuracy_scorer(model,
-                                            self.X_valid,
-                                            self.y_valid)
+        train_loss = 1 - self.accuracy_scorer(model, self.X_train[train_idx], self.y_train[train_idx])
+        val_loss = 1 - self.accuracy_scorer(model, self.X_valid, self.y_valid)
         cost = time.time() - start
 
         return {'function_value': val_loss,
@@ -122,9 +121,7 @@ class XGBoostBenchmark(AbstractBenchmark):
                 'subsample': subsample}
 
     @AbstractBenchmark._check_configuration
-    # @AbstractBenchmark._configuration_as_array
-    def objective_function_test(self, config: Dict, n_estimators: int,
-                                **kwargs) -> Dict:
+    def objective_function_test(self, config: Dict, n_estimators: int, **kwargs) -> Dict:
         """
         Trains a XGBoost model with a given configuration on both the train
         and validation data set and evaluates the model on the test data set.
@@ -162,8 +159,7 @@ class XGBoostBenchmark(AbstractBenchmark):
         return {'function_value': test_loss, 'cost': cost}
 
     @staticmethod
-    def get_configuration_space(seed: Union[int, None] = None
-                                ) -> CS.ConfigurationSpace:
+    def get_configuration_space(seed: Union[int, None] = None) -> CS.ConfigurationSpace:
         """
         Creates a ConfigSpace.ConfigurationSpace containing all parameters for
         the XGBoost Model
@@ -181,18 +177,12 @@ class XGBoostBenchmark(AbstractBenchmark):
         cs = CS.ConfigurationSpace(seed=seed)
 
         cs.add_hyperparameters([
-            CS.UniformFloatHyperparameter('eta',
-                                          lower=1e-3, upper=1, log=True),
-            CS.UniformFloatHyperparameter('min_child_weight',
-                                          lower=1e-10, upper=7., log=True),
-            CS.UniformFloatHyperparameter('colsample_bytree',
-                                          lower=0., upper=1.),
-            CS.UniformFloatHyperparameter('colsample_bylevel',
-                                          lower=0., upper=1.),
-            CS.UniformFloatHyperparameter('reg_lambda',
-                                          lower=2e-10, upper=2e10, log=True),
-            CS.UniformFloatHyperparameter('reg_alpha',
-                                          lower=2e-10, upper=2e10, log=True)
+            CS.UniformFloatHyperparameter('eta', lower=1e-3, upper=1, log=True),
+            CS.UniformFloatHyperparameter('min_child_weight', lower=1e-10, upper=7., log=True),
+            CS.UniformFloatHyperparameter('colsample_bytree', lower=0., upper=1.),
+            CS.UniformFloatHyperparameter('colsample_bylevel', lower=0., upper=1.),
+            CS.UniformFloatHyperparameter('reg_lambda', lower=2e-10, upper=2e10, log=True),
+            CS.UniformFloatHyperparameter('reg_alpha', lower=2e-10, upper=2e10, log=True)
         ])
 
         return cs
@@ -204,19 +194,18 @@ class XGBoostBenchmark(AbstractBenchmark):
                 'references': ['None'],
                 }
 
-    def _get_pipeline(self, eta: float, min_child_weight: int,
-                      colsample_bytree: float, colsample_bylevel: float,
-                      reg_lambda: int, reg_alpha: int,
-                      n_estimators: int) -> pipeline.Pipeline:
+    def _get_pipeline(self, eta: float, min_child_weight: int, colsample_bytree: float, colsample_bylevel: float,
+                      reg_lambda: int, reg_alpha: int, n_estimators: int) -> pipeline.Pipeline:
         """ Create the scikit-learn (training-)pipeline """
-        num_class = len(np.unique(self.y_train))
-        objective = 'binary:logistic' if num_class <= 2 else 'multi:softmax'
-        # in case of binary classification num_class has to be 1
-        num_class = 1 if num_class == 2 else num_class
+        objective = 'binary:logistic' if self.num_class <= 2 else 'multi:softmax'
 
         clf = pipeline.Pipeline(
             [('preprocess_impute',
               SimpleImputer(missing_values=np.nan, strategy='mean')),
+             ('preprocess_one_hot',
+              ColumnTransformer([
+                 ("categorical", OneHotEncoder(categories=self.categories, sparse=False), self.categorical_data),
+                 ("continuous", "passthrough", ~self.categorical_data)])),
              ('xgb', xgb.XGBClassifier(
                  learning_rate=eta,
                  min_child_weight=min_child_weight,
@@ -228,7 +217,6 @@ class XGBoostBenchmark(AbstractBenchmark):
                  objective=objective,
                  n_jobs=self.n_threads,
                  random_state=self.rng.randint(1, 100000),
-                 num_class=num_class,
-             ))
+                 num_class=self.num_class))
              ])
         return clf
