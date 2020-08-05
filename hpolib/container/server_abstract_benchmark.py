df2f160e3c6d3aab4e975227ee24ab61581586ee
PhMueller <muller-phil@gmx.net>
2020-08-05 18:02:09 +0200

    ADD full traceback if the server crashes

--- hpolib/container/server_abstract_benchmark.py
+++ hpolib/container/server_abstract_benchmark.py
@@ -69,10 +69,9 @@ class BenchmarkServer:
                 self.benchmark = Benchmark(**kwargs)  # noqa: F821
             else:
                 self.benchmark = Benchmark()  # noqa: F821
+            logger.info('Server: Connected Successfully')
         except Exception as e:
-            print(e)
-            logger.error(e)
-        logger.info('Server: Connected Successfully')
+            logger.exception(e)
 
     def get_configuration_space(self, kwargs_str: str) -> str:
         logger.debug(f'Server: get_config_space: kwargs_str: {kwargs_str}')
