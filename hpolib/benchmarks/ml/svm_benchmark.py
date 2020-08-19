9ae044b38560ea144479c615519847cdef874241
PhMueller <muller-phil@gmx.net>
2020-08-19 14:24:28 +0200

    ADD SVM Benchmark

---
+++ hpolib/benchmarks/ml/svm_benchmark.py
@@ -0,0 +1,316 @@
+import time
+from typing import Union, Tuple, Dict, List
+
+import numpy as np
+import ConfigSpace as CS
+
+from scipy import sparse
+from sklearn import svm
+from sklearn import pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.metrics import accuracy_score, make_scorer
+from sklearn.preprocessing import OneHotEncoder
+
+import hpolib.util.rng_helper as rng_helper
+from hpolib.abstract_benchmark import AbstractBenchmark
+from hpolib.util.openml_data_manager import OpenMLHoldoutDataManager
+
+__version__ = '0.0.1'
+
+
+class SupportVectorMachine(AbstractBenchmark):
+    """
+    Hyperparameter optimization task to optimize the regularization
+    parameter C and the kernel parameter gamma of a support vector machine.
+    Both hyperparameters are optimized on a log scale in [-10, 10].
+    The X_test data set is only used for a final offline evaluation of
+    a configuration. For that the validation and training data is
+    concatenated to form the whole training data set.
+    """
+
+    def __init__(self, task_id: Union[int, None] = None,
+                 rng: Union[np.random.RandomState, int, None] = None):
+        """
+        Parameters
+        ----------
+        task_id : int, None
+        rng : np.random.RandomState, int, None
+        """
+        super(SupportVectorMachine, self).__init__(rng=rng)
+
+        self.task_id = task_id
+        self.accuracy_scorer = make_scorer(accuracy_score)
+
+        self.X_train, self.y_train, self.X_valid, self.y_valid, self.X_test, self.y_test, variable_types = \
+            self.get_data()
+        self.categorical_data = np.array([var_type == 'categorical' for var_type in variable_types])
+
+        # Sort data (Categorical + numerical) so that categorical and continous are not mixed.
+        categorical_idx = np.argwhere(self.categorical_data)
+        continuous_idx = np.argwhere(~self.categorical_data)
+        sorting = np.concatenate([categorical_idx, continuous_idx]).squeeze()
+        self.categorical_data = self.categorical_data[sorting]
+        self.X_train = self.X_train[:, sorting]
+        self.X_valid = self.X_valid[:, sorting]
+        self.X_test = self.X_test[:, sorting]
+
+        nan_columns = np.all(np.isnan(self.X_train), axis=0)
+        self.categorical_data = self.categorical_data[~nan_columns]
+
+        mean_imputer = SimpleImputer(strategy='mean')
+        self.X_train = mean_imputer.fit_transform(self.X_train)
+        self.X_valid = mean_imputer.transform(self.X_valid)
+        self.X_test = mean_imputer.transform(self.X_test)
+
+        # Determine all possible values per categorical feature
+        complete_data = np.concatenate([self.X_train, self.X_valid, self.X_test], axis=0)
+        self.categories = [np.unique(complete_data[:, i])
+                           for i in range(self.X_train.shape[1]) if self.categorical_data[i]]
+
+        self.train_idx = self.rng.choice(a=np.arange(len(self.X_train)),
+                                         size=len(self.X_train),
+                                         replace=False)
+
+        # Use 10 time the number of classes as lower bound for the dataset fraction
+        n_classes = np.unique(self.y_train).shape[0]
+        self.lower_bound_train_size = int((10 * n_classes) / self.X_train.shape[0])
+
+    def get_data(self) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, np.ndarray, List]:
+        """ Loads the data given a task or another source. """
+
+        assert self.task_id is not None, NotImplementedError('No task-id given. Please either specify a task-id or '
+                                                             'overwrite the get_data method.')
+
+        dm = OpenMLHoldoutDataManager(openml_task_id=self.task_id, rng=self.rng)
+        X_train, y_train, X_val, y_val, X_test, y_test = dm.load()
+
+        return X_train, y_train, X_val, y_val, X_test, y_test, dm.variable_types
+
+    def shuffle_data(self, rng=None):
+        """ Reshuffle the training data. If 'rng' is None, the training idx are shuffled according to the
+        class-random-state"""
+        random_state = rng_helper.get_rng(rng, self.rng)
+        random_state.shuffle(self.train_idx)
+
+    @AbstractBenchmark._configuration_as_dict
+    @AbstractBenchmark._check_configuration
+    @AbstractBenchmark._check_fidelity
+    def objective_function(self, configuration: Union[Dict, CS.Configuration],
+                           fidelity: Union[Dict, None] = None,
+                           shuffle: bool = False,
+                           rng: Union[np.random.RandomState, int, None] = None, **kwargs) -> Dict:
+        """
+        Trains a SVM model given a hyperparameter configuration and
+        evaluates the model on the validation set.
+
+        Parameters
+        ----------
+        configuration : Dict, CS.Configuration
+            Configuration for the SVM model
+        fidelity: Dict, None
+            Fidelity parameters for the SVM model, check get_fidelity_space(). Uses default (max) value if None.
+        shuffle : bool
+            If ``True``, shuffle the training idx. If no parameter ``rng`` is given, use the class random state.
+            Defaults to ``False``.
+        rng : np.random.RandomState, int, None,
+            Random seed for benchmark. By default the class level random seed.
+
+            To prevent overfitting on a single seed, it is possible to pass a
+            parameter ``rng`` as 'int' or 'np.random.RandomState' to this function.
+            If this parameter is not given, the default random state is used.
+        kwargs
+
+        Returns
+        -------
+        Dict -
+            function_value : validation loss
+            cost : time to train and evaluate the model
+            info : Dict
+                train_loss : training loss
+                fidelity : used fidelities in this evaluation
+        """
+        start_time = time.time()
+
+        self.rng = rng_helper.get_rng(rng=rng, self_rng=self.rng)
+
+        if shuffle:
+            self.shuffle_data(self.rng)
+
+        # Split of dataset subset
+        train_size = max(fidelity['dataset_fraction'],
+                         self.lower_bound_train_size)
+        train_size = int(train_size * len(self.train_idx))
+
+        train_idx = self.train_idx[:train_size]
+        train = self.X_train[train_idx]
+        train_targets = self.y_train[train_idx]
+
+        # Transform hyperparameters to linear scale
+        hp_c = np.exp(float(configuration['C']))
+        hp_gamma = np.exp(float(configuration['gamma']))
+
+        # Train support vector machine
+        model = self.get_pipeline(hp_c, hp_gamma)
+        model.fit(train, train_targets)
+
+        # Compute validation error
+        train_loss = 1 - self.accuracy_scorer(model, self.X_train[train_idx], self.y_train[train_idx])
+        val_loss = 1 - self.accuracy_scorer(model, self.X_valid, self.y_valid)
+
+        cost = time.time() - start_time
+
+        return {'function_value': val_loss,
+                "cost": cost,
+                'info': {'train_loss': train_loss,
+                         'fidelity': fidelity['dataset_fraction']}}
+
+
+    @AbstractBenchmark._configuration_as_dict
+    @AbstractBenchmark._check_configuration
+    @AbstractBenchmark._check_fidelity
+    def objective_function_test(self, configuration: Union[Dict, CS.Configuration],
+                                fidelity: Union[Dict, None] = None, shuffle: bool = False,
+                                rng: Union[np.random.RandomState, int, None] = None, **kwargs) -> Dict:
+        """
+        Trains a SVM model with a given configuration on both the X_train
+        and validation data set and evaluates the model on the X_test data set.
+
+        Parameters
+        ----------
+        configuration : Dict, CS.Configuration
+            Configuration for the SVM Model
+        fidelity: Dict, None
+            Fidelity parameters, check get_fidelity_space(). Uses default (max) value if None.
+        rng : np.random.RandomState, int, None,
+            Random seed for benchmark. By default the class level random seed.
+            To prevent overfitting on a single seed, it is possible to pass a
+            parameter ``rng`` as 'int' or 'np.random.RandomState' to this function.
+            If this parameter is not given, the default random state is used.
+        kwargs
+
+        Returns
+        -------
+        Dict -
+            function_value : X_test loss
+            cost : time to X_train and evaluate the model
+            info : Dict
+                train_valid_loss: Loss on the train+valid data set
+                fidelity : used fidelities in this evaluation
+        """
+        assert np.isclose(fidelity['dataset_fraction'], 1), f'Data set fraction must be 1 but was {fidelity["dataset_fraction"]}'
+
+        start_time = time.time()
+
+        self.rng = rng_helper.get_rng(rng=rng, self_rng=self.rng)
+
+        # Concatenate training and validation dataset
+        if isinstance(self.X_train, sparse.csr.csr_matrix) or isinstance(self.X_valid, sparse.csr.csr_matrix):
+            data = sparse.vstack((self.X_train, self.X_valid))
+        else:
+            data = np.concatenate((self.X_train, self.X_valid))
+        targets = np.concatenate((self.y_train, self.y_valid))
+
+        # Transform hyperparameters to linear scale
+        hp_c = np.exp(float(configuration['C']))
+        hp_gamma = np.exp(float(configuration['gamma']))
+
+        model = self.get_pipeline(hp_c, hp_gamma)
+        model.fit(data, targets)
+
+        # Compute validation error
+        train_valid_loss = 1 - self.accuracy_scorer(model, data, targets)
+
+        # Compute test error
+        test_loss = 1 - self.accuracy_scorer(model, self.X_test, self.y_test)
+
+        cost = time.time() - start_time
+
+        return {'function_value': test_loss,
+                "cost": cost,
+                'info': {'train_valid_loss': train_valid_loss,
+                         'fidelity': fidelity['dataset_fraction']}}
+
+    def get_pipeline(self, C: float, gamma: float) -> pipeline.Pipeline:
+        """ Create the scikit-learn (training-)pipeline """
+
+        model = pipeline.Pipeline([
+            ('preprocess_one_hot',
+             ColumnTransformer([
+                 ("categorical", OneHotEncoder(categories=self.categories, sparse=False), self.categorical_data),
+                 ("continuous", "passthrough", ~self.categorical_data)])),
+            ('svm',
+             svm.SVC(gamma=gamma, C=C, random_state=self.rng))
+        ])
+        return model
+
+    @staticmethod
+    def get_configuration_space(seed: Union[int, None] = None) -> CS.ConfigurationSpace:
+        """
+        Creates a ConfigSpace.ConfigurationSpace containing all parameters for
+        the SVM Model
+
+        For a detailed explanation of the hyperparameters:
+        https://scikit-learn.org/stable/modules/generated/sklearn.svm.SVC.html
+
+        Parameters
+        ----------
+        seed : int, None
+            Fixing the seed for the ConfigSpace.ConfigurationSpace
+
+        Returns
+        -------
+        ConfigSpace.ConfigurationSpace
+        """
+
+        seed = seed if seed is not None else np.random.randint(1, 100000)
+        cs = CS.ConfigurationSpace(seed=seed)
+
+        cs.add_hyperparameters([
+            CS.UniformFloatHyperparameter('C', lower=-10., upper=10., default_value=0., log=False),
+            CS.UniformFloatHyperparameter('gamma', lower=-10., upper=10., default_value=1., log=False),
+        ])
+        # cs.generate_all_continuous_from_bounds(SupportVectorMachine.get_meta_information()['bounds'])
+        return cs
+
+    @staticmethod
+    def get_fidelity_space(seed: Union[int, None] = None) -> CS.ConfigurationSpace:
+        """
+        Creates a ConfigSpace.ConfigurationSpace containing all fidelity parameters for
+        the SupportVector Benchmark
+
+        Fidelities
+        ----------
+        dataset_fraction: float - [0.1, 1]
+            fraction of training data set to use
+
+        Parameters
+        ----------
+        seed : int, None
+            Fixing the seed for the ConfigSpace.ConfigurationSpace
+
+        Returns
+        -------
+        ConfigSpace.ConfigurationSpace
+        """
+        seed = seed if seed is not None else np.random.randint(1, 100000)
+        fidel_space = CS.ConfigurationSpace(seed=seed)
+
+        fidel_space.add_hyperparameters([
+            CS.UniformFloatHyperparameter("dataset_fraction", lower=0.1, upper=1.0, default_value=1.0, log=False),
+        ])
+        return fidel_space
+
+    @staticmethod
+    def get_meta_information():
+        """ Returns the meta information for the benchmark """
+
+        return {'name': 'Support Vector Machine',
+                'references': ["@article{klein-corr16,"
+                               "author = {A. Klein and S. Falkner and S. Bartels and P. Hennig and F. Hutter},"
+                               "title = {Fast Bayesian Optimization of Machine Learning"
+                               "Hyperparameters on Large Datasets},"
+                               "journal = corr,"
+                               "llvolume = {abs/1605.07079},"
+                               "lurl = {http://arxiv.org/abs/1605.07079}, year = {2016} }"]
+                }
---
+++ tests/test_svm.py
@@ -0,0 +1,38 @@
+import pytest
+
+from hpolib.benchmarks.ml.svm_benchmark import SupportVectorMachine
+from hpolib.util.openml_data_manager import get_openmlcc18_taskids
+
+task_ids = get_openmlcc18_taskids()
+
+
+def test_svm_init():
+    benchmark = SupportVectorMachine(task_id=task_ids[0])
+
+    fs = benchmark.get_fidelity_space(seed=0)
+    fidelity = fs.sample_configuration().get_dictionary()
+    assert fidelity['dataset_fraction'] == pytest.approx(0.5939321535345923, abs=0.001)
+
+    meta = benchmark.get_meta_information()
+    assert meta is not None
+
+    cs = benchmark.get_configuration_space(seed=0)
+    config = cs.sample_configuration().get_dictionary()
+    print(config)
+    assert config['C'] == pytest.approx(0.9762, abs=0.001)
+    assert config['gamma'] == pytest.approx(4.3037, abs=0.001)
+
+    result = benchmark.objective_function(configuration=config, fidelity=fidelity)
+    assert result['function_value'] == pytest.approx(0.4837, abs=0.1)
+    assert result['cost'] is not None
+
+    with pytest.raises(AssertionError):
+        result = benchmark.objective_function_test(configuration=config, fidelity=fidelity)
+
+    result = benchmark.objective_function_test(configuration=config)
+    assert result['function_value'] == pytest.approx(0.4648, abs=0.1)
+    assert result['cost'] is not None
+
+
+if __name__ == "__main__":
+    test_svm_init()
