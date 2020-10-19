140be7cf5bab87681feaed71c2f81b42b685e2f5
Archit Bansal <archit.bansal.93@gmail.com>
2020-10-19 16:21:43 +0200

    FIXED Benchmark interface issues, refer #46 and #49.
    Signed-off-by: Archit Bansal <archit.bansal.93@gmail.com>

--- hpolib/abstract_benchmark.py
+++ hpolib/abstract_benchmark.py
@@ -1,7 +1,8 @@
 """ Base-class of all benchmarks """
 
 import abc
-from typing import Union, Dict
+from typing import Union, Dict, List
+import functools
 
 import logging
 import ConfigSpace
@@ -38,7 +39,8 @@ class AbstractBenchmark(object, metaclass=abc.ABCMeta):
         self.fidelity_space = self.get_fidelity_space()
 
     @abc.abstractmethod
-    def objective_function(self, configuration: Dict, fidelity: Union[Dict, None] = None,
+    def objective_function(self, configuration: Union[np.ndarray, List, ConfigSpace.Configuration, Dict],
+                           fidelity: Union[Dict, ConfigSpace.Configuration, None] = None,
                            rng: Union[np.random.RandomState, int, None] = None,
                            *args, **kwargs) -> dict:
         """
@@ -70,7 +72,8 @@ class AbstractBenchmark(object, metaclass=abc.ABCMeta):
         pass
 
     @abc.abstractmethod
-    def objective_function_test(self, configuration: Dict, fidelity: Union[Dict, None] = None,
+    def objective_function_test(self, configuration: Union[np.ndarray, List, ConfigSpace.Configuration, Dict],
+                                fidelity: Union[Dict, ConfigSpace.Configuration, None] = None,
                                 rng: Union[np.random.RandomState, int, None] = None,
                                 *args, **kwargs) -> Dict:
         """
@@ -103,10 +106,13 @@ class AbstractBenchmark(object, metaclass=abc.ABCMeta):
 
         Can be combined with the _configuration_as_array decorator.
         """
-        def wrapper(self, configuration: Union[np.ndarray, ConfigSpace.Configuration, Dict], **kwargs):
+
+        # Copy all documentation from the underlying function except the annotations.
+        @functools.wraps(wrapped=foo, assigned=('__module__', '__name__', '__qualname__', '__doc__',))
+        def wrapper(self, configuration: Union[np.ndarray, List, ConfigSpace.Configuration, Dict], **kwargs):
 
             try:
-                if isinstance(configuration, np.ndarray):
+                if isinstance(configuration, (np.ndarray, List)):
                     config_dict = {k: configuration[i] for (i, k) in enumerate(self.configuration_space)}
                     config = ConfigSpace.Configuration(self.configuration_space, config_dict)
                 elif isinstance(configuration, dict):
@@ -142,6 +148,9 @@ class AbstractBenchmark(object, metaclass=abc.ABCMeta):
         Order independent from the _check_configuration decorator, but it does forward all fidelity parameters,
         regardless of input, as a dictionary in the 'fidelity' keyword argument.
         """
+
+        # Copy all documentation from the underlying function except the annotations.
+        @functools.wraps(wrapped=foo, assigned=('__module__', '__name__', '__qualname__', '__doc__',))
         def wrapper(self, configuration: Union[np.ndarray, ConfigSpace.Configuration, Dict],
                     fidelity: Union[Dict, ConfigSpace.Configuration, None] = None, **kwargs):
 
@@ -171,8 +180,8 @@ class AbstractBenchmark(object, metaclass=abc.ABCMeta):
                 raise e
 
             if fidelity is None:
-                raise TypeError(f'Configuration has to be from type np.ndarray, dict, or ConfigSpace.Configuration but '
-                                f'was {type(configuration)}')
+                raise TypeError(f'Fidelity has to be an instance of type np.ndarray, dict, or '
+                                f'ConfigSpace.Configuration but was {type(configuration)}')
 
             # Ensure that the extracted fidelity values play well with the defined fidelity space
             self.fidelity_space.check_configuration(fidelity)
