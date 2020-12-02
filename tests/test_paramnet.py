af6acdf472d5ef9d8c4e8ecb84d78a70ac61ace2
PhMueller <muller-phil@gmx.net>
2020-12-02 18:04:04 +0100

    [skip travis] Update Tests + Add Container support

--- tests/test_paramnet.py
+++ tests/test_paramnet.py
@@ -21,9 +21,9 @@ def test_load_data():
 
 def test_obj_func():
 
-    from hpobench.benchmarks.surrogates.paramnet_benchmark import ParamnetBenchmark
+    from hpobench.container.benchmarks.surrogates.paramnet_benchmark import ParamNetHiggsOnStepsBenchmark
 
-    benchmark = ParamnetBenchmark(dataset='higgs')
+    benchmark = ParamNetHiggsOnStepsBenchmark()
     cs = benchmark.get_configuration_space(0)
     fs = benchmark.get_fidelity_space(0)
     cfg = cs.sample_configuration()
@@ -31,14 +31,32 @@ def test_obj_func():
     result = benchmark.objective_function(cfg)
     assert result['function_value'] == pytest.approx(0.3244, 0.01)
 
+    full_budget = fs.get_default_configuration()
+    assert full_budget['step'] == 50
+
+    result_on_full_budget = benchmark.objective_function(configuration=cfg, fidelity=full_budget)
+    assert result['function_value'] == pytest.approx(result_on_full_budget['function_value'], abs=0.000001)
+
+    result_on_small_budget = benchmark.objective_function(configuration=cfg, fidelity={'step': 1})
+    assert result['cost'] == pytest.approx(result_on_small_budget['cost'] * 50, abs=0.0001)
+
 
 def test_param_net_time():
-    from hpobench.benchmarks.surrogates.paramnet_benchmark import ParamnetTimeBenchmark
-    benchmark = ParamnetTimeBenchmark(dataset='higgs')
+    from hpobench.container.benchmarks.surrogates.paramnet_benchmark import ParamNetHiggsOnTimeBenchmark
+
+    benchmark = ParamNetHiggsOnTimeBenchmark()
     cs = benchmark.get_configuration_space(0)
-    fs = benchmark.get_fidelity_space(0)
     cfg = cs.sample_configuration()
 
     result = benchmark.objective_function(cfg)
     assert result['function_value'] == pytest.approx(0.3244, 0.01)
     assert isinstance(result['info']['learning_curve'], list)
+
+    assert result['info']['observed_epochs'] == 193
+    assert result['info']['learning_curve'][49] == result['info']['learning_curve'][192]
+
+    # Test the case when the budget is less than the costs for 50 epochs
+    fidelity = {'budget': 50}
+    result = benchmark.objective_function(configuration=cfg, fidelity=fidelity)
+    assert result['info']['observed_epochs'] == 38
+    assert len(result['info']['learning_curve']) == 38
