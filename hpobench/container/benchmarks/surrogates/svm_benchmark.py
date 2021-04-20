67c55f117c28dedefe6b75ccb5afceee1e80e3ab
PhMueller <muller-phil@gmx.net>
2021-04-20 12:32:32 +0200

    SVM: Add Container Recipe + ClientInterface

---
+++ hpobench/container/benchmarks/surrogates/svm_benchmark.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+""" Benchmark for the svm surrogates Benchmark from hpobench/benchmarks/surrogates/svm_benchmark.py
+"""
+
+from hpobench.container.client_abstract_benchmark import AbstractBenchmarkClient
+
+
+class SurrogateSVMBenchmark(AbstractBenchmarkClient):
+    def __init__(self, **kwargs):
+        kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'SurrogateSVMBenchmark')
+        kwargs['container_name'] = kwargs.get('container_name', 'surrogatessvm')
+        kwargs['latest'] = kwargs.get('container_tag', '0.0.1')
+        super(SurrogateSVMBenchmark, self).__init__(**kwargs)
