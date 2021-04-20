d9d5d776668906fb17c5f15d40ff071a14606df1
PhMueller <muller-phil@gmx.net>
2021-04-20 12:46:42 +0200

    Rename svm surrogate container

--- hpobench/container/benchmarks/surrogates/svm_benchmark.py
+++ hpobench/container/benchmarks/surrogates/svm_benchmark.py
@@ -10,6 +10,6 @@ from hpobench.container.client_abstract_benchmark import AbstractBenchmarkClient
 class SurrogateSVMBenchmark(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'SurrogateSVMBenchmark')
-        kwargs['container_name'] = kwargs.get('container_name', 'surrogatessvm')
+        kwargs['container_name'] = kwargs.get('container_name', 'surrogate_svm')
         kwargs['latest'] = kwargs.get('container_tag', '0.0.1')
         super(SurrogateSVMBenchmark, self).__init__(**kwargs)
