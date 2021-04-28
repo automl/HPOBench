68455248148c4a4705bd75c2b8759368cd072a76
PhMueller <muller-phil@gmx.net>
2021-04-28 08:46:35 +0200

    Supress some flake warnings

--- hpobench/__init__.py
+++ hpobench/__init__.py
@@ -4,8 +4,7 @@ _default_log_format = '[%(levelname)s] %(name)s at %(asctime)s --- %(message)s'
 logging.basicConfig(format=_default_log_format, level=logging.WARNING)
 root_logger = logging.getLogger()
 
-from hpobench.__version__ import __version__  # noqa: F401
-from hpobench.config import config_file  # noqa: F401
-
+from hpobench.__version__ import __version__  # noqa: F401, E402
+from hpobench.config import config_file  # noqa: F401, E402
 
 __contact__ = "automl.org"
