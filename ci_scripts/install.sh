1ecc108fb0db7d5a678e8046bed5add13a5bdb68
PhMueller <muller-phil@gmx.net>
2021-05-05 16:51:14 +0200

    Fix Github cant download release

--- ci_scripts/install.sh
+++ ci_scripts/install.sh
@@ -56,8 +56,8 @@ if [[ "$USE_SINGULARITY" == "true" ]]; then
       cryptsetup
 
     export VERSION=3.5.3 && # adjust this as necessary \
-      wget https://github.com/sylabs/singularity/releases/download/v${VERSION}/singularity-${VERSION}.tar.gz && \
-      tar -xzf singularity-${VERSION}.tar.gz && \
+      wget https://github.com/sylabs/singularity/archive/refs/tags/v${VERSION}.tar.gz
+      tar -xzf v${VERSION}.tar.gz && \
       cd singularity
 
     ./mconfig && \
