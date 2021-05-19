23df7213377540d561086db0881569d5a5b61ab6
PhMueller <muller-phil@gmx.net>
2021-05-19 16:11:39 +0200

    Init Commit: Rover Benchmark.

---
+++ hpobench/benchmarks/rl/rover_benchmark.py
@@ -0,0 +1,254 @@
+"""
+This benchmark is from https://github.com/zi-w/Ensemble-Bayesian-Optimization/tree/master/test_functions
+And is used in
+- "Batched large-scale bayesian optimization in high-dimensional spaces", Wang et al.
+- "Scalable Global Optimization via Local Bayesian Optimization", Eriksson et al.
+
+How to use this benchmark:
+--------------------------
+
+We recommend using the containerized version of this benchmark.
+If you want to use this benchmark locally (without running it via the corresponding container),
+you need to perform the following steps.
+
+Prerequisites:
+==============
+Conda environment in which the HPOBench is installed (pip install .). Activate your environment.
+```
+conda activate <Name_of_Conda_HPOBench_environment>
+```
+
+Description of the benchmark:
+=============================
+
+From Batched large-scale bayesian optimization in high-dimensional spaces:
+"... we consider a trajectory optimization task in 2D, meant to emulate a rover navigation task.
+We describe a problem instance by defining a start position s and a goal position g as well as a cost function
+over the state space. Trajectories are described by a set of points on which a BSpline is to be fitted.
+By integrating the cost function over a given trajectory, we can compute the trajectory cost c(x) of a given trajectory
+solution x ∈ [0, 1]^60."
+
+
+1. Clone from github:
+=====================
+```
+git clone git@github.com:automl/HPOBench.git
+```
+
+2. Clone and install
+====================
+```
+cd /path/to/HPOBench
+pip install .
+
+```
+
+Changelog:
+==========
+0.0.1:
+* Initial implementation
+"""
+
+import logging
+from typing import Union, Dict
+
+import ConfigSpace as CS
+import numpy as np
+
+from hpobench.abstract_benchmark import AbstractBenchmark
+from hpobench.util.benchmarks.rl.rover_utils import get_obstacle_positions, AABoxes, NegGeom, UnionGeom, \
+    ConstObstacleCost, ConstCost, AdditiveCosts, PointBSpline, RoverDomain, NormalizedInputFn, ConstantOffsetFn
+
+__version__ = '0.0.1'
+
+logger = logging.getLogger('RoverBenchmark')
+
+
+class RoverBenchmark(AbstractBenchmark):
+
+    N_COORDINATES = 30
+
+    def __init__(self, rng: Union[np.random.RandomState, int, None] = None):
+        """
+        Parameters
+        ----------
+        rng : np.random.RandomState, int, None
+        """
+        super(RoverBenchmark, self).__init__(rng=rng)
+
+        self.domain = self.create_large_domain(force_start=False,
+                                               force_goal=False,
+                                               start_miss_cost=self.l2cost,
+                                               goal_miss_cost=self.l2cost,
+                                               rng=self.rng)
+
+    @AbstractBenchmark.check_parameters
+    def objective_function(self, configuration: Union[CS.Configuration, Dict],
+                           fidelity: Union[CS.Configuration, Dict, None] = None,
+                           rng: Union[np.random.RandomState, int, None] = None, **kwargs) -> Dict:
+        from time import time
+        start_time = time()
+
+        configuration = self.configuration_to_array(configuration)
+
+        f = ConstantOffsetFn(self.domain, offset=5)
+        f = NormalizedInputFn(f, self.domain.s_range)
+
+        obj_value = f(configuration)
+
+        return {'function_value': obj_value,
+                "cost": time() - start_time,
+                'info': {'fidelity': fidelity}}
+
+    @AbstractBenchmark.check_parameters
+    def objective_function_test(self, configuration: Union[CS.Configuration, Dict],
+                                fidelity: Union[CS.Configuration, Dict, None] = None,
+                                rng: Union[np.random.RandomState, int, None] = None, **kwargs) -> Dict:
+
+        return self.objective_function(configuration, fidelity=fidelity, rng=rng)
+
+    @staticmethod
+    def get_configuration_space(seed: Union[int, None] = None) -> CS.ConfigurationSpace:
+        """
+        A configuration in this benchmark is a set of 30 coordinates (60 points) defining the trajectory of the rover.
+        A B-Spline is created from those coordinates, which represents then the route of the rover.
+
+        We define here the 30 coordinates by 60 parameter. The coordinates are constructed from the values as follows:
+
+        Values / Parameter:     coord_1_x, coord_1_y, coord_2_x, coord_2_y, ..., coord_30_y
+        Resulting Coordinates:  (coord_1_x, coord_1_y), (coord_2_x, coord_2_y), ..., (coord_30_x, coord_30_y)
+
+        Parameters
+        ----------
+        seed : int, None
+            Fixing the seed for the ConfigSpace.ConfigurationSpace
+
+        Returns
+        -------
+        ConfigSpace.ConfigurationSpace
+        """
+        seed = seed if seed is not None else np.random.randint(1, 100000)
+        cs = CS.ConfigurationSpace(seed=seed)
+
+        cs.add_hyperparameters([CS.UniformFloatHyperparameter(f'coord_{index}_{p}', lower=0, upper=1, log=False)
+                                for index in range(1, RoverBenchmark.N_COORDINATES + 1) for p in ['x', 'y']])
+        return cs
+
+    @staticmethod
+    def get_fidelity_space(seed: Union[int, None] = None) -> CS.ConfigurationSpace:
+        """
+        Creates a ConfigSpace.ConfigurationSpace for the rover benchmark.
+
+        Note that this benchmark has no fidelities! We return an empty configspace!
+
+        Parameters
+        ----------
+        seed : int, None
+            Fixing the seed for the ConfigSpace.ConfigurationSpace
+
+        Returns
+        -------
+        ConfigSpace.ConfigurationSpace
+        """
+        seed = seed if seed is not None else np.random.randint(1, 100000)
+        fidel_space = CS.ConfigurationSpace(seed=seed)
+        return fidel_space
+
+    @staticmethod
+    def get_meta_information():
+        """ Returns the meta information for the benchmark """
+        return {'name': 'Rover Benchmark',
+                'references': ["@inproceedings{wang2018batched,  "
+                               "title        = {Batched large-scale bayesian optimization in high-dimensional spaces},"
+                               "author       = {Wang, Zi and Gehring, Clement and Kohli, Pushmeet and "
+                               "                Jegelka, Stefanie},"
+                               "booktitle    = {International Conference on Artificial Intelligence and Statistics},"
+                               "pages        = {745--754},"
+                               "year         = {2018},"
+                               "organization = {PMLR}}",
+
+                               "@inproceedings{eriksson2019scalable,"
+                               "title     = {Scalable Global Optimization via Local {Bayesian} Optimization},"
+                               "author    = {Eriksson, David and Pearce, Michael and Gardner, Jacob and Turner, Ryan D "
+                               "             and Poloczek, Matthias},"
+                               "booktitle = {Advances in Neural Information Processing Systems},"
+                               "pages     = {5496--5507},"
+                               "year      = {2019},"
+                               "url       = {http://papers.nips.cc/paper/"
+                               "             8788-scalable-global-optimization-via-local-bayesian-optimization.pdf}}"
+                               ],
+                'code': ['https://github.com/zi-w/Ensemble-Bayesian-Optimization',
+                         'https://github.com/uber-research/TuRBO']
+                }
+
+    @staticmethod
+    def create_large_cost_function():
+        # The center coordinates of some obstacles
+        c = get_obstacle_positions()
+
+        # Each obstacle is a box defined by two coordinates.
+        # left lower corner: [center_x - 0.025, center_y - 0.025]
+        # right upper corner: [center_x + 0.025, center_y + 0.025]
+        low = c - 0.025
+        high = c + 0.025
+
+        # Create a map on which the rover should stay. lower left: [0, 0], upper right: [1, 1]
+        r_box = np.array([[0.5, 0.5]])
+        r_l = r_box - 0.5
+        r_h = r_box + 0.5
+
+        # Create the obstacles as Bounding Boxes
+        trees = AABoxes(low, high)
+
+        # If the Rover is not (!) in the world, then we have higher costs (that's why they use here "negative geometry")
+        r_box = NegGeom(AABoxes(r_l, r_h))
+
+        obstacles = UnionGeom([trees, r_box])
+
+        # Define fix start and goal points
+        start = np.zeros([0.05, 0.05])
+        goal = np.array([0.95, 0.95])
+
+        # The costs for hitting a obstacle is 20, having a longer trajectory (route) is 0.05
+        costs = [ConstObstacleCost(obstacles, cost=20.), ConstCost(0.05)]
+        cost_fn = AdditiveCosts(costs)
+        return cost_fn, start, goal
+
+    @staticmethod
+    def create_large_domain(force_start=False,
+                            force_goal=False,
+                            start_miss_cost=None,
+                            goal_miss_cost=None,
+                            rng=None):
+
+        cost_fn, start, goal = RoverBenchmark.create_large_cost_function()
+
+        traj = PointBSpline(dim=2, num_points=RoverBenchmark.N_COORDINATES)
+
+        domain = RoverDomain(cost_fn,
+                             start=start,
+                             goal=goal,
+                             traj=traj,
+                             start_miss_cost=start_miss_cost,
+                             goal_miss_cost=goal_miss_cost,
+                             force_start=force_start,
+                             force_goal=force_goal,
+                             s_range=np.array([[-0.1, -0.1], [1.1, 1.1]]),
+                             rnd_stream=rng)
+
+        return domain
+
+    @staticmethod
+    def configuration_to_array(configuration: Union[CS.Configuration, Dict]):
+        if isinstance(configuration, CS.Configuration):
+            return configuration.get_array()
+        elif isinstance(configuration, Dict):
+            configuration_array = np.zeros((RoverBenchmark.N_COORDINATES, 2))
+            for coord_no in range(1, RoverBenchmark.N_COORDINATES):
+                configuration_array[coord_no - 1, 0] = configuration[f'coord_{coord_no}_x']
+                configuration_array[coord_no - 1, 1] = configuration[f'coord_{coord_no}_y']
+            return configuration_array
+
+    @staticmethod
+    def l2cost(x, point):
+        return 10 * np.linalg.norm(x - point, 1)
---
+++ hpobench/util/benchmarks/rl/rover_utils.py
@@ -0,0 +1,455 @@
+import numpy as np
+import scipy.interpolate as si
+
+
+def get_obstacle_positions():
+    c = np.array([[0.43143755, 0.20876147],
+                  [0.38485367, 0.39183579],
+                  [0.02985961, 0.22328303],
+                  [0.7803707, 0.3447003],
+                  [0.93685657, 0.56297285],
+                  [0.04194252, 0.23598362],
+                  [0.28049582, 0.40984475],
+                  [0.6756053, 0.70939481],
+                  [0.01926493, 0.86972335],
+                  [0.5993437, 0.63347932],
+                  [0.57807619, 0.40180792],
+                  [0.56824287, 0.75486851],
+                  [0.35403502, 0.38591056],
+                  [0.72492026, 0.59969313],
+                  [0.27618746, 0.64322757],
+                  [0.54029566, 0.25492943],
+                  [0.30903526, 0.60166842],
+                  [0.2913432, 0.29636879],
+                  [0.78512072, 0.62340245],
+                  [0.29592116, 0.08400595],
+                  [0.87548394, 0.04877622],
+                  [0.21714791, 0.9607346],
+                  [0.92624074, 0.53441687],
+                  [0.53639253, 0.45127928],
+                  [0.99892031, 0.79537837],
+                  [0.84621631, 0.41891986],
+                  [0.39432819, 0.06768617],
+                  [0.92365693, 0.72217512],
+                  [0.95520914, 0.73956575],
+                  [0.820383, 0.53880139],
+                  [0.22378049, 0.9971974],
+                  [0.34023233, 0.91014706],
+                  [0.64960636, 0.35661133],
+                  [0.29976464, 0.33578931],
+                  [0.43202238, 0.11563227],
+                  [0.66764947, 0.52086962],
+                  [0.45431078, 0.94582745],
+                  [0.12819915, 0.33555344],
+                  [0.19287232, 0.8112075],
+                  [0.61214791, 0.71940626],
+                  [0.4522542, 0.47352186],
+                  [0.95623345, 0.74174186],
+                  [0.17340293, 0.89136853],
+                  [0.04600255, 0.53040724],
+                  [0.42493468, 0.41006649],
+                  [0.37631485, 0.88033853],
+                  [0.66951947, 0.29905739],
+                  [0.4151516, 0.77308712],
+                  [0.55762991, 0.26400156],
+                  [0.6280609, 0.53201974],
+                  [0.92727447, 0.61054975],
+                  [0.93206587, 0.42107549],
+                  [0.63885574, 0.37540613],
+                  [0.15303425, 0.57377797],
+                  [0.8208471, 0.16566631],
+                  [0.14889043, 0.35157346],
+                  [0.71724622, 0.57110725],
+                  [0.32866327, 0.8929578],
+                  [0.74435871, 0.47464421],
+                  [0.9252026, 0.21034329],
+                  [0.57039306, 0.54356078],
+                  [0.56611551, 0.02531317],
+                  [0.84830056, 0.01180542],
+                  [0.51282028, 0.73916524],
+                  [0.58795481, 0.46527371],
+                  [0.83259048, 0.98598188],
+                  [0.00242488, 0.83734691],
+                  [0.72505789, 0.04846931],
+                  [0.07312971, 0.30147979],
+                  [0.55250344, 0.23891255],
+                  [0.51161315, 0.46466442],
+                  [0.802125, 0.93440495],
+                  [0.9157825, 0.32441602],
+                  [0.44927665, 0.53380074],
+                  [0.67708372, 0.67527231],
+                  [0.81868924, 0.88356194],
+                  [0.48228814, 0.88668497],
+                  [0.39805433, 0.99341196],
+                  [0.86671752, 0.79016975],
+                  [0.01115417, 0.6924913],
+                  [0.34272199, 0.89543756],
+                  [0.40721675, 0.86164495],
+                  [0.26317679, 0.37334193],
+                  [0.74446787, 0.84782643],
+                  [0.55560143, 0.46405104],
+                  [0.73567977, 0.12776233],
+                  [0.28080322, 0.26036748],
+                  [0.17507419, 0.95540673],
+                  [0.54233783, 0.1196808],
+                  [0.76670967, 0.88396285],
+                  [0.61297539, 0.79057776],
+                  [0.9344029, 0.86252764],
+                  [0.48746839, 0.74942784],
+                  [0.18657635, 0.58127321],
+                  [0.10377802, 0.71463978],
+                  [0.7771771, 0.01463505],
+                  [0.7635042, 0.45498358],
+                  [0.83345861, 0.34749363],
+                  [0.38273809, 0.51890558],
+                  [0.33887574, 0.82842507],
+                  [0.02073685, 0.41776737],
+                  [0.68754547, 0.96430979],
+                  [0.4704215, 0.92717361],
+                  [0.72666234, 0.63241306],
+                  [0.48494401, 0.72003268],
+                  [0.52601215, 0.81641253],
+                  [0.71426732, 0.47077212],
+                  [0.00258906, 0.30377501],
+                  [0.35495269, 0.98585155],
+                  [0.65507544, 0.03458909],
+                  [0.10550588, 0.62032937],
+                  [0.60259145, 0.87110846],
+                  [0.04959159, 0.535785]])
+    return c
+
+
+class Trajectory:
+
+    def __init__(self):
+        pass
+
+    def set_params(self, start, goal, params):
+        raise NotImplemented
+
+    def get_points(self, t):
+        raise NotImplemented
+
+    @property
+    def param_size(self):
+        raise NotImplemented
+
+
+class PointBSpline(Trajectory):
+    """
+    dim : number of dimensions of the state space
+    num_points : number of internal points used to represent the trajectory.
+                    Note, internal points are not necessarily on the trajectory.
+    """
+
+    def __init__(self, dim, num_points):
+        super().__init__()
+        self.tck = None
+        self.d = dim
+        self.npoints = num_points
+
+    def set_params(self, params, start=None, goal=None):
+        """
+        Set fit the parameters of the spline from a set of points. If values are given for start or goal,
+        the start or endpoint of the trajectory will be forces on those points, respectively.
+        """
+        points = params.reshape((-1, self.d)).T
+        self.current_points = points
+
+        if start is not None:
+            points = np.hstack((start[:, None], points))
+
+        if goal is not None:
+            points = np.hstack((points, goal[:, None]))
+
+        self.tck, u = si.splprep(points, k=3)
+
+        if start is not None:
+            for a, sv in zip(self.tck[1], start):
+                a[0] = sv
+
+        if goal is not None:
+            for a, gv in zip(self.tck[1], goal):
+                a[-1] = gv
+
+    def get_points(self, t):
+        assert self.tck is not None, "Parameters have to be set with set_params() before points can be queried."
+        return np.vstack(si.splev(t, self.tck)).T
+
+    @property
+    def param_size(self):
+        return self.d * self.npoints
+
+
+def simple_rbf(x, point):
+    return (1 - np.exp(-np.sum(((x - point) / 0.25) ** 2)))
+
+
+class RoverDomain:
+    """
+    Rover domain defined on R^d
+    cost_fn : vectorized function giving a scalar cost to states
+    start : a start state for the rover
+    goal : a goal state
+    traj : a parameterized trajectory object offering an interface
+            to interpolate point on the trajectory
+    s_range : the min and max of the state with s_range[0] in R^d are
+                the mins and s_range[1] in R^d are the maxs
+    """
+
+    def __init__(self, cost_fn,
+                 start,
+                 goal,
+                 traj,
+                 s_range,
+                 start_miss_cost=None,
+                 goal_miss_cost=None,
+                 force_start=True,
+                 force_goal=True,
+                 only_add_start_goal=True,
+                 rnd_stream=None):
+        self.cost_fn = cost_fn
+        self.start = start
+        self.goal = goal
+        self.traj = traj
+        self.s_range = s_range
+        self.rnd_stream = rnd_stream
+        self.force_start = force_start
+        self.force_goal = force_goal
+
+        self.goal_miss_cost = goal_miss_cost
+        self.start_miss_cost = start_miss_cost
+
+        if self.start_miss_cost is None:
+            self.start_miss_cost = simple_rbf
+        if self.goal_miss_cost is None:
+            self.goal_miss_cost = simple_rbf
+
+        if self.rnd_stream is None:
+            self.rnd_stream = np.random.RandomState(np.random.randint(0, 2 ** 32 - 1))
+
+    # return the negative cost which need to be optimized
+    def __call__(self, params, n_samples=1000):
+        self.set_params(params)
+
+        return -self.estimate_cost(n_samples=n_samples)
+
+    def set_params(self, params):
+        self.traj.set_params(params + self.rnd_stream.normal(0, 1e-4, params.shape),
+                             self.start if self.force_start else None,
+                             self.goal if self.force_goal else None)
+
+    def estimate_cost(self, n_samples=1000):
+        # get points on the trajectory
+        points = self.traj.get_points(np.linspace(0, 1.0, n_samples, endpoint=True))
+        # compute cost at each point
+        costs = self.cost_fn(points)
+
+        # estimate (trapezoidal) the integral of the cost along traj
+        avg_cost = 0.5 * (costs[:-1] + costs[1:])
+        l = np.linalg.norm(points[1:] - points[:-1], axis=1)
+        total_cost = np.sum(l * avg_cost)
+
+        if not self.force_start:
+            total_cost += self.start_miss_cost(points[0], self.start)
+        if not self.force_goal:
+            total_cost += self.goal_miss_cost(points[-1], self.goal)
+
+        return total_cost
+
+    @property
+    def input_size(self):
+        return self.traj.param_size
+
+
+class AABoxes:
+    def __init__(self, lows, highs):
+        self.l = lows
+        self.h = highs
+
+    def contains(self, X):
+        if X.ndim == 1:
+            X = X[None, :]
+
+        lX = self.l.T[None, :, :] <= X[:, :, None]
+        hX = self.h.T[None, :, :] > X[:, :, None]
+
+        return lX.all(axis=1) & hX.all(axis=1)
+
+
+class NegGeom:
+    def __init__(self, geometry):
+        self.geom = geometry
+
+    def contains(self, X):
+        return ~self.geom.contains(X)
+
+
+class UnionGeom:
+    def __init__(self, geometries):
+        self.geoms = geometries
+
+    def contains(self, X):
+        return np.any(np.hstack([g.contains(X) for g in self.geoms]), axis=1, keepdims=True)
+
+
+class ConstObstacleCost:
+    def __init__(self, geometry, cost):
+        self.geom = geometry
+        self.c = cost
+
+    def __call__(self, X):
+        return self.c * self.geom.contains(X)
+
+
+class ConstCost:
+    def __init__(self, cost):
+        self.c = cost
+
+    def __call__(self, X):
+        if X.ndim == 1:
+            X = X[None, :]
+        return np.ones((X.shape[0], 1)) * self.c
+
+
+class AdditiveCosts:
+    def __init__(self, fns):
+        self.fns = fns
+
+    def __call__(self, X):
+        return np.sum(np.hstack([f(X) for f in self.fns]), axis=1)
+
+
+class GMCost:
+    def __init__(self, centers, sigmas, weights=None):
+        self.c = centers
+        self.s = sigmas
+        if self.s.ndim == 1:
+            self.s = self.s[:, None]
+        self.w = weights
+        if weights is None:
+            self.w = np.ones(centers.shape[0])
+
+    def __call__(self, X):
+        if X.ndim == 1:
+            X = X[None, :]
+
+        return np.exp(-np.sum(((X[:, :, None] - self.c.T[None, :, :]) / self.s.T[None, :, :]) ** 2, axis=1)).dot(self.w)
+
+
+def plot_2d_rover(roverdomain, ngrid_points=100, ntraj_points=100, colormap='RdBu', draw_colorbar=False):
+    import matplotlib.pyplot as plt
+    # get a grid of points over the state space
+    points = [np.linspace(mi, ma, ngrid_points, endpoint=True) for mi, ma in zip(*roverdomain.s_range)]
+    grid_points = np.meshgrid(*points)
+    points = np.hstack([g.reshape((-1, 1)) for g in grid_points])
+
+    # compute the cost at each point on the grid
+    costs = roverdomain.cost_fn(points)
+
+    # get the cost of the current trajectory
+    traj_cost = roverdomain.estimate_cost()
+
+    # get points on the current trajectory
+    traj_points = roverdomain.traj.get_points(np.linspace(0., 1.0, ntraj_points, endpoint=True))
+
+    # set title to be the total cost
+    plt.title('traj cost: {0}'.format(traj_cost))
+    print('traj cost: {0}'.format(traj_cost))
+    # plot cost function
+    cmesh = plt.pcolormesh(grid_points[0], grid_points[1], costs.reshape((ngrid_points, -1)), cmap=colormap)
+    if draw_colorbar:
+        plt.gcf().colorbar(cmesh)
+    # plot traj
+    plt.plot(traj_points[:, 0], traj_points[:, 1], 'g')
+    # plot start and goal
+    plt.plot([roverdomain.start[0], roverdomain.goal[0]], (roverdomain.start[1], roverdomain.goal[1]), 'ok')
+
+    return cmesh
+
+
+def generate_verts(rectangles):
+    poly3d = []
+    all_faces = []
+    vertices = []
+    for l, h in zip(rectangles.l, rectangles.h):
+        verts = [[l[0], l[1], l[2]], [l[0], h[1], l[2]], [h[0], h[1], l[2]], [h[0], l[1], l[2]],
+                 [l[0], l[1], h[2]], [l[0], h[1], h[2]], [h[0], h[1], h[2]], [h[0], l[1], h[2]]]
+
+        faces = [[0, 1, 2, 3], [0, 3, 7, 4], [3, 2, 6, 7], [7, 6, 5, 4], [1, 5, 6, 2], [0, 4, 5, 1]]
+
+        vert_ind = [[0, 1, 2], [0, 2, 3], [0, 3, 4], [4, 3, 7], [7, 3, 2], [2, 6, 7],
+                    [7, 5, 4], [7, 6, 5], [2, 5, 6], [2, 1, 5], [0, 1, 4], [1, 4, 5]]
+
+        plist = [[verts[vert_ind[ix][iy]] for iy in range(len(vert_ind[0]))] for ix in range(len(vert_ind))]
+        faces = [[verts[faces[ix][iy]] for iy in range(len(faces[0]))] for ix in range(len(faces))]
+
+        poly3d = poly3d + plist
+        vertices = vertices + verts
+        all_faces = all_faces + faces
+
+    return poly3d, vertices, all_faces
+
+
+def plot_3d_forest_rover(roverdomain, rectangles, ntraj_points=100):
+    from matplotlib import pyplot as plt
+    from mpl_toolkits.mplot3d.art3d import Poly3DCollection, Line3DCollection
+
+    # get the cost of the current trajectory
+    traj_cost = roverdomain.estimate_cost()
+
+    # get points on the current trajectory
+    traj_points = roverdomain.traj.get_points(np.linspace(0., 1.0, ntraj_points, endpoint=True))
+
+    # convert the rectangles into lists of vertices for matplotlib
+    poly3d, verts, faces = generate_verts(rectangles)
+
+    ax = plt.gcf().add_subplot(111, projection='3d')
+
+    # plot start and goal
+    ax.scatter((roverdomain.start[0], roverdomain.goal[0]),
+               (roverdomain.start[1], roverdomain.goal[1]),
+               (roverdomain.start[2], roverdomain.goal[2]), c='k')
+
+    # plot traj
+    seg = zip(traj_points[:-1, :], traj_points[1:, :])
+    ax.add_collection3d(Line3DCollection(seg, colors=[(0, 1., 0, 1.)] * len(seg)))
+
+    # plot rectangles
+    ax.add_collection3d(Poly3DCollection(poly3d, facecolors=(0.7, 0.7, 0.7, 1.), linewidth=0.5))
+
+    # set limits of axis to be the same as domain
+    s_range = roverdomain.s_range
+    ax.set_xlim(s_range[0][0], s_range[1][0])
+    ax.set_ylim(s_range[0][1], s_range[1][1])
+    ax.set_zlim(s_range[0][2], s_range[1][2])
+
+
+class NormalizedInputFn:
+    def __init__(self, fn_instance, x_range):
+        self.fn_instance = fn_instance
+        self.x_range = x_range
+
+    def __call__(self, x):
+        return self.fn_instance(self.project_input(x))
+
+    def project_input(self, x):
+        return x * (self.x_range[1] - self.x_range[0]) + self.x_range[0]
+
+    def inv_project_input(self, x):
+        return (x - self.x_range[0]) / (self.x_range[1] - self.x_range[0])
+
+    def get_range(self):
+        return np.array([np.zeros(self.x_range[0].shape[0]), np.ones(self.x_range[0].shape[0])])
+
+
+class ConstantOffsetFn:
+    def __init__(self, fn_instance, offset):
+        self.fn_instance = fn_instance
+        self.offset = offset
+
+    def __call__(self, x):
+        return self.fn_instance(x) + self.offset
+
+    def get_range(self):
+        return self.fn_instance.get_range()
