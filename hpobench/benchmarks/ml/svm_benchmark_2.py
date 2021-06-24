4d1d2d6a8e0a9de88dcf21af6bf07196eeadb69a
neeratyoy <neeratyoy@gmail.com>
2021-06-24 18:21:34 +0200

    Minor code cleaning

--- hpobench/benchmarks/ml/svm_benchmark_2.py
+++ hpobench/benchmarks/ml/svm_benchmark_2.py
@@ -16,7 +16,6 @@ from sklearn.pipeline import make_pipeline, Pipeline
 from sklearn.metrics import accuracy_score, make_scorer
 
 import hpobench.util.rng_helper as rng_helper
-from hpobench.abstract_benchmark import AbstractBenchmark
 from hpobench.benchmarks.ml.ml_benchmark_template import Benchmark
 
 
