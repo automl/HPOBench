22df34afa5ff3df2cfd6a20b0b97a8031a6ddb22
renesass <mail@renesass.de>
2021-07-06 12:53:55 +0200

    Updated classnames and filenames

--- hpobench/container/benchmarks/od/od_kde.py
+++ hpobench/container/benchmarks/od/od_kde.py
@@ -6,10 +6,10 @@
 from hpobench.container.client_abstract_benchmark import AbstractBenchmarkClient
 
 
-class ODAutoencoder(AbstractBenchmarkClient):
+class ODKernelDensityEstimation(AbstractBenchmarkClient):
     def __init__(self, dataset_name: str, **kwargs):
         kwargs['dataset_name'] = dataset_name
-        kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'ODAutoencoder')
-        kwargs['container_name'] = kwargs.get('container_name', 'ae_benchmark')
+        kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'ODKernelDensityEstimation')
+        kwargs['container_name'] = kwargs.get('container_name', 'od_kde')
         kwargs['latest'] = kwargs.get('container_tag', '0.0.1')
-        super(ODAutoencoder, self).__init__(**kwargs)
+        super(ODKernelDensityEstimation, self).__init__(**kwargs)
