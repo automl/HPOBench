48af58decdc329924ff6425f7193551cf3fa0d10
PhMueller <muller-phil@gmx.net>
2021-08-16 23:45:17 +0200

    Fix minor errors.

--- hpobench/benchmarks/ml_mmfb/entry_point.py
+++ hpobench/benchmarks/ml_mmfb/entry_point.py
@@ -10,3 +10,15 @@ from hpobench.benchmarks.ml_mmfb.svm_benchmark import SVMSearchSpace0Benchmark, 
 from hpobench.benchmarks.ml_mmfb.tabular_benchmark import TabularBenchmark, OriginalTabularBenchmark
 from hpobench.benchmarks.ml_mmfb.xgboost_benchmark import XGBoostSearchSpace0Benchmark, XGBoostSearchSpace1Benchmark,\
     XGBoostSearchSpace2Benchmark, XGBoostSearchSpace3Benchmark
+
+
+__all__ = [HistGBSearchSpace0Benchmark, HistGBSearchSpace1Benchmark, HistGBSearchSpace2Benchmark,
+           HistGBSearchSpace3Benchmark,
+           LRSearchSpace0Benchmark, LRSearchSpace1Benchmark, LRSearchSpace2Benchmark, LRSearchSpace3Benchmark,
+           NNSearchSpace0Benchmark, NNSearchSpace1Benchmark, NNSearchSpace2Benchmark, NNSearchSpace3Benchmark,
+           RandomForestSearchSpace0Benchmark, RandomForestSearchSpace1Benchmark, RandomForestSearchSpace2Benchmark,
+           RandomForestSearchSpace3Benchmark,
+           SVMSearchSpace0Benchmark, SVMSearchSpace1Benchmark,
+           TabularBenchmark, OriginalTabularBenchmark,
+           XGBoostSearchSpace0Benchmark, XGBoostSearchSpace1Benchmark, XGBoostSearchSpace2Benchmark,
+           XGBoostSearchSpace3Benchmark]
