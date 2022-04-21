17a3a879a8f0e8568c6b8c29d9a36ad608a12075
pfistfl <pfistererf@googlemail.com>
2022-04-21 17:34:35 +0200

    remove example

--- hpobench/container/benchmarks/surrogates/yahpo_gym.py
+++ hpobench/container/benchmarks/surrogates/yahpo_gym.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-""" Benchmark for the YAHPO surrogates Benchmark from hpobench/benchmarks/surrogates/yahpo_gym.py
-Test with
-from hpobench.container.benchmarks.surrogates.yahpo_gym import YAHPOGymBenchmark
-b = YAHPOGymBenchmark(container_source=".", container_name="yahpo_gym", scenario = "lcbench", instance = "3945", objective ="val_accuracy")
-res = b.objective_function(configuration=b.get_configuration_space(seed=1).sample_configuration())
-"""
-
 from hpobench.container.client_abstract_benchmark import AbstractBenchmarkClient
-
 
 class YAHPOGymBenchmark(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
