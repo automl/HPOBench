b70756a25aca17fa79acf9706933ac8c4806d667
Philipp Müller <muller-phil@gmx.net>
2022-05-01 11:57:53 +0200

    Update hpobench/container/benchmarks/surrogates/yahpo_gym.py

--- hpobench/container/benchmarks/surrogates/yahpo_gym.py
+++ hpobench/container/benchmarks/surrogates/yahpo_gym.py
@@ -3,6 +3,7 @@
 
 from hpobench.container.client_abstract_benchmark import AbstractBenchmarkClient
 
+
 class YAHPOGymBenchmark(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'YAHPOGymBenchmark')
