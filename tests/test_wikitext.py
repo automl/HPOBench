6e6af739857e8745423fbe3532b0848060336058
ayushi-3536 <ayushi.sharma.3536@gmail.com>
2022-05-21 10:13:28 +0200

    -make deterministic -report train and eval time separately in objective func -code formatting -added test file

---
+++ tests/test_wikitext.py
@@ -0,0 +1,35 @@
+import logging
+import pytest
+
+logging.basicConfig(level=logging.DEBUG)
+
+
+def test_wikitext_benchmark():
+    from hpobench.benchmarks.mo.lm_benchmark import LanguageModelBenchmark
+
+    # Check Seeding
+    benchmark = LanguageModelBenchmark(rng=0)
+    cs = benchmark.get_configuration_space(seed=1)
+    cfg_1 = cs.sample_configuration()
+
+    cs = benchmark.get_configuration_space(seed=1)
+    cfg_2 = cs.sample_configuration()
+
+    assert cfg_1 == cfg_2
+
+    print("cfg1", cfg_1)
+    print("cfg2", cfg_2)
+
+
+    test_config = {
+        'batch_size': 144, 'clip': 1.458859796107597, 'dropout': 0.5967357423109274,
+        'emsize': 575, 'lr': 5.245378070737081, 'lr_factor': 15
+    }
+
+    result_1 = benchmark.objective_function(test_config, rng=1, fidelity={'budget': 1})
+    result_2 = benchmark.objective_function(test_config, rng=1, fidelity={'budget': 1})
+    print("r1", result_1)
+    print("r2", result_2)
+
+    assert result_1['info']['train_accuracy'] == pytest.approx(0.76145, rel=0.001)
+    assert result_1['info']['train_accuracy'] == result_2['info']['train_accuracy']
