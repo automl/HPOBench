5d3d75ec1da14bb31dc0caffc0f47df1efab3e02
Florian <pfistfl@users.noreply.github.com>
2022-05-30 13:20:13 +0100

    Add YAHPO Benchmark (#142)
    * Add yahpo_gym w help from phmueller
    
    Co-authored-by: PhMueller <muller-phil@gmx.net>

---
+++ hpobench/container/benchmarks/surrogates/yahpo_gym.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+from hpobench.container.client_abstract_benchmark import AbstractBenchmarkClient, AbstractMOBenchmarkClient
+
+
+class YAHPOGymBenchmark(AbstractBenchmarkClient):
+    def __init__(self, **kwargs):
+        kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'YAHPOGymBenchmark')
+        kwargs['container_name'] = kwargs.get('container_name', 'yahpo_gym')
+        kwargs['latest'] = kwargs.get('container_tag', '0.0.1')
+        super(YAHPOGymBenchmark, self).__init__(**kwargs)
+
+
+class YAHPOGymMOBenchmark(AbstractMOBenchmarkClient):
+    def __init__(self, **kwargs):
+        kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'YAHPOGymMOBenchmark')
+        kwargs['container_name'] = kwargs.get('container_name', 'yahpo_gym')
+        kwargs['latest'] = kwargs.get('container_tag', '0.0.1')
+        super(YAHPOGymMOBenchmark, self).__init__(**kwargs)
