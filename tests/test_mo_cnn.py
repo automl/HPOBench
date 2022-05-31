c7ec82737e03f4d3619780c9da22619bc3acf022
Philipp Müller <muller-phil@gmx.net>
2022-05-31 12:19:37 +0200

    Update tests

--- tests/test_mo_cnn.py
+++ tests/test_mo_cnn.py
@@ -27,5 +27,7 @@ def test_mo_cnn_benchmark():
     result_1 = benchmark.objective_function(test_config, rng=1, fidelity={'budget': 3})
     result_2 = benchmark.objective_function(test_config, rng=1, fidelity={'budget': 3})
 
-    assert result_1['info']['train_accuracy'] == pytest.approx(0.1029, rel=0.001)
+    assert result_1['info']['valid_accuracy'] == pytest.approx(0.1029, rel=0.001)
+    assert result_1['info']['valid_accuracy'] == result_1['function_value']['accuracy']
+    assert result_1['info']['train_accuracy'] == pytest.approx(0.1044, rel=0.001)
     assert result_1['info']['train_accuracy'] == result_2['info']['train_accuracy']
