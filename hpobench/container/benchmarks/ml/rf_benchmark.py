fd25d57e846bf3e39b313820a9ad39558f52bd2d
neeratyoy <neeratyoy@gmail.com>
2022-06-02 16:24:50 +0200

    Updating container information for ML benches

--- hpobench/container/benchmarks/ml/rf_benchmark.py
+++ hpobench/container/benchmarks/ml/rf_benchmark.py
@@ -6,27 +6,31 @@
 from hpobench.container.client_abstract_benchmark import AbstractBenchmarkClient
 
 
+container_name = "ml_mfbb"
+container_version = "0.0.3"
+
+
 class RandomForestBenchmark(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'RandomForestBenchmark')
-        kwargs['container_name'] = kwargs.get('container_name', 'ml_mmfb')
-        kwargs['latest'] = kwargs.get('container_tag', '0.0.1')
+        kwargs['container_name'] = kwargs.get('container_name', container_name)
+        kwargs['latest'] = kwargs.get('container_tag', container_version)
         super(RandomForestBenchmark, self).__init__(**kwargs)
 
 
 class RandomForestBenchmarkBB(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'RandomForestBenchmarkBB')
-        kwargs['container_name'] = kwargs.get('container_name', 'ml_mmfb')
-        kwargs['latest'] = kwargs.get('container_tag', '0.0.1')
+        kwargs['container_name'] = kwargs.get('container_name', container_name)
+        kwargs['latest'] = kwargs.get('container_tag', container_version)
         super(RandomForestBenchmarkBB, self).__init__(**kwargs)
 
 
 class RandomForestBenchmarkMF(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'RandomForestBenchmarkMF')
-        kwargs['container_name'] = kwargs.get('container_name', 'ml_mmfb')
-        kwargs['latest'] = kwargs.get('container_tag', '0.0.1')
+        kwargs['container_name'] = kwargs.get('container_name', container_name)
+        kwargs['latest'] = kwargs.get('container_tag', container_version)
         super(RandomForestBenchmarkMF, self).__init__(**kwargs)
 
 
