ff1c7411b007bee626a504fa4c0b5e77bce4f39e
neeratyoy <neeratyoy@gmail.com>
2022-06-06 11:11:12 +0200

    Making MO work with containers

--- hpobench/container/benchmarks/ml/__init__.py
+++ hpobench/container/benchmarks/ml/__init__.py
@@ -1,18 +1,31 @@
-from hpobench.container.benchmarks.ml.histgb_benchmark import HistGBBenchmark, HistGBBenchmarkBB, HistGBBenchmarkMF
-from hpobench.container.benchmarks.ml.lr_benchmark import LRBenchmark, LRBenchmarkBB, LRBenchmarkMF
-from hpobench.container.benchmarks.ml.nn_benchmark import NNBenchmark, NNBenchmarkBB, NNBenchmarkMF
-from hpobench.container.benchmarks.ml.rf_benchmark import RandomForestBenchmark, RandomForestBenchmarkBB, \
-    RandomForestBenchmarkMF
-from hpobench.container.benchmarks.ml.svm_benchmark import SVMBenchmark, SVMBenchmarkBB, SVMBenchmarkMF
-from hpobench.container.benchmarks.ml.tabular_benchmark import TabularBenchmark
-from hpobench.container.benchmarks.ml.xgboost_benchmark import XGBoostBenchmark, XGBoostBenchmarkBB, XGBoostBenchmarkMF
-from hpobench.container.benchmarks.ml.yahpo_benchmark import YAHPOGymRawBenchmark, YAHPOGymMORawBenchmark
+from hpobench.container.benchmarks.ml.lr_benchmark import LRBenchmark, LRBenchmarkBB, \
+    LRBenchmarkMF, LRMOBenchmark, LRMOBenchmarkBB, LRMOBenchmarkMF
+from hpobench.container.benchmarks.ml.nn_benchmark import NNBenchmark, NNBenchmarkBB, \
+    NNBenchmarkMF, NNMOBenchmark, NNMOBenchmarkBB, NNMOBenchmarkMF
+from hpobench.container.benchmarks.ml.rf_benchmark import RandomForestBenchmark, \
+    RandomForestBenchmarkBB, RandomForestBenchmarkMF, RandomForestMOBenchmark, \
+    RandomForestMOBenchmarkBB, RandomForestMOBenchmarkMF
+from hpobench.container.benchmarks.ml.svm_benchmark import SVMBenchmark, SVMBenchmarkBB, \
+    SVMBenchmarkMF, SVMMOBenchmark, SVMMOBenchmarkBB, SVMMOBenchmarkMF
+from hpobench.container.benchmarks.ml.tabular_benchmark import TabularBenchmark, TabularMOBenchmark
+from hpobench.container.benchmarks.ml.xgboost_benchmark import XGBoostBenchmark, \
+    XGBoostBenchmarkBB, XGBoostBenchmarkMF, XGBoostMOBenchmark, XGBoostMOBenchmarkBB, \
+    XGBoostMOBenchmarkMF
+from hpobench.container.benchmarks.ml.yahpo_benchmark import YAHPOGymRawBenchmark, \
+    YAHPOGymMORawBenchmark
 
-__all__ = ['HistGBBenchmark', 'HistGBBenchmarkBB', 'HistGBBenchmarkMF',
-           'LRBenchmark', 'LRBenchmarkBB', 'LRBenchmarkMF',
-           'NNBenchmark', 'NNBenchmarkBB', 'NNBenchmarkMF',
-           'RandomForestBenchmark', 'RandomForestBenchmarkBB', 'RandomForestBenchmarkMF',
-           'SVMBenchmark', 'SVMBenchmarkBB', 'SVMBenchmarkMF',
-           'TabularBenchmark',
-           'XGBoostBenchmark', 'XGBoostBenchmarkBB', 'XGBoostBenchmarkMF',
-           'YAHPOGymRawBenchmark', 'YAHPOGymMORawBenchmark']
+
+__all__ = [
+    'LRBenchmark', 'LRBenchmarkBB', 'LRBenchmarkMF',
+    'LRMOBenchmark', 'LRMOBenchmarkBB', 'LRMOBenchmarkMF',
+    'NNBenchmark', 'NNBenchmarkBB', 'NNBenchmarkMF',
+    'NNMOBenchmark', 'NNMOBenchmarkBB', 'NNMOBenchmarkMF',
+    'RandomForestBenchmark', 'RandomForestBenchmarkBB', 'RandomForestBenchmarkMF',
+    'RandomForestMOBenchmark', 'RandomForestMOBenchmarkBB', 'RandomForestMOBenchmarkMF',
+    'SVMBenchmark', 'SVMBenchmarkBB', 'SVMBenchmarkMF',
+    'SVMMOBenchmark', 'SVMMOBenchmarkBB', 'SVMMOBenchmarkMF',
+    'TabularBenchmark', 'TabularMOBenchmark',
+    'XGBoostBenchmark', 'XGBoostBenchmarkBB', 'XGBoostBenchmarkMF',
+    'XGBoostMOBenchmark', 'XGBoostMOBenchmarkBB', 'XGBoostMOBenchmarkMF',
+    'YAHPOGymRawBenchmark', 'YAHPOGymMORawBenchmark'
+]
