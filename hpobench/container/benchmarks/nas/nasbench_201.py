203134492ca190007d06a92d57d3d7b480283260
PhMueller <muller-phil@gmx.net>
2022-12-15 11:34:52 +0100

    Update 201 version 0.0.6
    - link to the correct container version

--- hpobench/container/benchmarks/nas/nasbench_201.py
+++ hpobench/container/benchmarks/nas/nasbench_201.py
@@ -10,7 +10,7 @@ class Cifar10ValidNasBench201Benchmark(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'Cifar10ValidNasBench201Benchmark')
         kwargs['container_name'] = kwargs.get('container_name', 'nasbench_201')
-        kwargs['latest'] = kwargs.get('container_tag', '0.0.5')
+        kwargs['latest'] = kwargs.get('container_tag', '0.0.6')
         super(Cifar10ValidNasBench201Benchmark, self).__init__(**kwargs)
 
 
@@ -18,7 +18,7 @@ class Cifar100NasBench201Benchmark(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'Cifar100NasBench201Benchmark')
         kwargs['container_name'] = kwargs.get('container_name', 'nasbench_201')
-        kwargs['latest'] = kwargs.get('container_tag', '0.0.5')
+        kwargs['latest'] = kwargs.get('container_tag', '0.0.6')
         super(Cifar100NasBench201Benchmark, self).__init__(**kwargs)
 
 
@@ -26,7 +26,7 @@ class ImageNetNasBench201Benchmark(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'ImageNetNasBench201Benchmark')
         kwargs['container_name'] = kwargs.get('container_name', 'nasbench_201')
-        kwargs['latest'] = kwargs.get('container_tag', '0.0.5')
+        kwargs['latest'] = kwargs.get('container_tag', '0.0.6')
         super(ImageNetNasBench201Benchmark, self).__init__(**kwargs)
 
 
@@ -34,7 +34,7 @@ class Cifar10ValidNasBench201BenchmarkOriginal(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'Cifar10ValidNasBench201BenchmarkOriginal')
         kwargs['container_name'] = kwargs.get('container_name', 'nasbench_201')
-        kwargs['latest'] = kwargs.get('container_tag', '0.0.5')
+        kwargs['latest'] = kwargs.get('container_tag', '0.0.6')
         super(Cifar10ValidNasBench201BenchmarkOriginal, self).__init__(**kwargs)
 
 
@@ -42,7 +42,7 @@ class Cifar100NasBench201BenchmarkOriginal(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'Cifar100NasBench201BenchmarkOriginal')
         kwargs['container_name'] = kwargs.get('container_name', 'nasbench_201')
-        kwargs['latest'] = kwargs.get('container_tag', '0.0.5')
+        kwargs['latest'] = kwargs.get('container_tag', '0.0.6')
         super(Cifar100NasBench201BenchmarkOriginal, self).__init__(**kwargs)
 
 
@@ -50,7 +50,7 @@ class ImageNetNasBench201BenchmarkOriginal(AbstractBenchmarkClient):
     def __init__(self, **kwargs):
         kwargs['benchmark_name'] = kwargs.get('benchmark_name', 'ImageNetNasBench201BenchmarkOriginal')
         kwargs['container_name'] = kwargs.get('container_name', 'nasbench_201')
-        kwargs['latest'] = kwargs.get('container_tag', '0.0.5')
+        kwargs['latest'] = kwargs.get('container_tag', '0.0.6')
         super(ImageNetNasBench201BenchmarkOriginal, self).__init__(**kwargs)
 
 
